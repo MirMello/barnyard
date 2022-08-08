5474185d550d2e45293d01bd77646fd05cf799e7
Alex Canez <el.winy.36@gmail.com>
2022-08-08 15:00:18 -0600

    added post react

---
+++ my-app/src/components/pages/Post.js
@@ -0,0 +1,56 @@
+import Header from "../Header";
+import React, { useState } from 'react';
+import { validatePost } from '../../utils/helpers';
+
+function Post() {
+    const [postState, setPostState] = useState({ message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
+    const { message } = postState;
+    const newPost = (e) => {
+        if (e.target.name === 'Post') {
+            const isValid = validatePost(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your post is empty.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setPostState({ ...postState, [e.target.name]: e.target.value });
+            console.log('New Post', postState);
+        }
+    };
+
+
+    return (
+        <div>
+            <Header title="Post in your Barnyard" />
+            <div>
+                <h3>Create a Post:</h3>
+                <div>
+                    <label for="message">Message:</label>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={newPost} />
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">
+                    Submit
+                </button>
+            </div>
+            <section>
+                 <h3>Post Feed:</h3>
+            </section>
+        </div>
+    )
+}
+
+export default Post;
