b110443de02fc0e0111571f61dcf80f1dcea24b0
Alex Standfield <AlexStandfield@gmail.com>
2022-08-08 21:06:47 -0600

    Initial Models Created

---
+++ models/Animals.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const AnimalSchema = new Schema({
+    animalId: {
+        type: Schema.Types.ObjectId
+    },
+    barnId: {
+        type: Number
+    },
+    name: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    picture: {
+        type: String
+    },
+    posts: {
+        type: subSchema
+    }
+});
