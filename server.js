c36422f65c47c3d527ef9e70a0fe4cbea63ce72b
MirMello <mirandaleeallen@gmail.com>
2022-08-15 18:48:55 -0600

    put all files back into parent folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+// const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
@@ -7,11 +7,11 @@ const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
 
 const app = express();
 
