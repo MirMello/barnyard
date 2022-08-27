1335f2221e3a35a3aa395f4d3074d4138f810a84
MirMello <mirandaleeallen@gmail.com>
2022-08-27 09:31:37 -0600

    fixing missing css

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,75 @@
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+// import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import logo from './logo.svg';
+// import './App.css';
+// import { useState } from 'react';
+// // import logo from './logo.svg';
+// // import animals from './components/animals';
+// // import Events from './components/Events';
+// // import pictures from './components/pictures';
+// // import Post from './components/Post';
+// import Barns from './pages/Barns';
+// import Stalls from './pages/Stalls';
+// import Addanimal from './pages/Addanimal';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import NavBar from "./components/Navbar";
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   const [currentPage, setcurrentPage] = useState('Barns')
+//   return (
+//     <ApolloProvider client={client}>
+//     <div>
+//       <nav>
+//         <ul style={{ display: 'flex', listStyle: 'none' }}>
+//           <li id='title'>Barnyard</li>
+//           <button onClick = {()=>setcurrentPage("Login")}type="login-button">Login</button>
+//           <button onClick = {()=>setcurrentPage("Signup")}type="sign-up-button">Sign Up</button>
+//         </ul>
+//       </nav>
+//       {currentPage === "Stalls" && <Stalls />}
+//        {currentPage === "Addanimal" && <Addanimal setcurrentPage={setcurrentPage}/>}
+//          {currentPage === "Barns" && <Barns setcurrentPage={setcurrentPage}/>}
+//          {currentPage === "Login" && <Login setcurrentPage={setcurrentPage}/>}
+//          {currentPage === "Signup" && <Signup setcurrentPage={setcurrentPage}/>}
+//     </div>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import logo from './logo.svg';
 // import animals from './components/animals';
 // import Events from './components/Events';
 // import pictures from './components/pictures';
 // import Post from './components/Post';
-import Barns from './pages/Barns';
-import Stalls from './pages/Stalls';
-import Addanimal from './pages/Addanimal';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Barns from "./pages/Barns";
+import Stalls from "./pages/Stalls";
+import Addanimal from "./pages/Addanimal";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NavBar from "./components/Navbar";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const client = new ApolloClient({
@@ -23,23 +78,18 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const [currentPage, setcurrentPage] = useState('Barns')
   return (
     <ApolloProvider client={client}>
-    <div>
-      <nav>
-        <ul style={{ display: 'flex', listStyle: 'none' }}>
-          <li id='title'>Barnyard</li>
-          <button onClick = {()=>setcurrentPage("Login")}type="login-button">Login</button>
-          <button onClick = {()=>setcurrentPage("Signup")}type="sign-up-button">Sign Up</button>
-        </ul>
-      </nav>
-      {currentPage === "Stalls" && <Stalls />}
-       {currentPage === "Addanimal" && <Addanimal setcurrentPage={setcurrentPage}/>}
-         {currentPage === "Barns" && <Barns setcurrentPage={setcurrentPage}/>}
-         {currentPage === "Login" && <Login setcurrentPage={setcurrentPage}/>}
-         {currentPage === "Signup" && <Signup setcurrentPage={setcurrentPage}/>}
-    </div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/stalls" element={<Stalls />} />
+          <Route path="/addanimal" element={<Addanimal />} />
+          <Route path="/barns" element={<Barns />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <h1>BarnYard</h1>
-      <ul>
+      <ul style={{ display: 'flex', listStyle: 'none' }}>
         <li><a href='/login'><button type="button">Login</button></a></li>
         <li><a href='/signup'><button type="button">Sign Up</button></a></li>
         <li><a href='/barns'><button type="button">Barns</button></a></li>
@@ -14,3 +14,6 @@ function Navbar() {
 };
 
 export default Navbar;
+
+        // <button onClick={() => setcurrentPage("Login")} type="login-button">Login</button>
+        // <button onClick={() => setcurrentPage("Signup")} type="sign-up-button">Sign Up</button>
