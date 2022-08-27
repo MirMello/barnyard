fd0cc9e2d1c183e3f496c2127c608ddc4588b003
MirMello <mirandaleeallen@gmail.com>
2022-08-27 09:22:10 -0600

    fixing missing css

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function Navbar(){
-    return(
-        <nav className= "navbar">
-          <h1>Barnyard</h1>
-        <ul style={{ display: 'flex', listStyle: 'none' }}>
-          <li><a href='/login'><button type="button">Login</button></a></li>
-          <li><a href='/signup'><button type="button">Sign Up</button></a></li>
-          <li><a href='/barns'><button type="button">Barns</button></a></li>
-        </ul>
-      </nav>
-    )
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>BarnYard</h1>
+      <ul style={{ display: 'flex', listStyle: 'none' }}>
+        <li><a href='/login'><button type="button">Login</button></a></li>
+        <li><a href='/signup'><button type="button">Sign Up</button></a></li>
+        <li><a href='/barns'><button type="button">Barns</button></a></li>
+      </ul>
+    </nav>
+  )
 };
 
 export default Navbar;
